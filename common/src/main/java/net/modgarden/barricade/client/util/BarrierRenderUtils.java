a821bb20e454a9d79ebc527b5d01d099a6832bad
Pug <81167232+MerchantPug@users.noreply.github.com>
2024-08-11 15:16:34 +1000

    Fix server-sided crash.

--- common/src/main/java/net/modgarden/barricade/client/util/BarrierRenderUtils.java
+++ common/src/main/java/net/modgarden/barricade/client/util/BarrierRenderUtils.java
@@ -8,6 +8,11 @@ import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.client.renderer.texture.TextureAtlas;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.core.SectionPos;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.component.BlockItemStateProperties;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.chunk.LevelChunk;
 import net.modgarden.barricade.client.model.CreativeOnlyBakedModelAccess;
 import net.modgarden.barricade.mixin.client.ClientChunkCacheAccessor;
@@ -45,7 +50,24 @@ public class BarrierRenderUtils {
                 .setLight(15728880);
     }
 
-    public static void reloadBarrierBlocks() {
+    public static void refreshBarrierBlocks(ItemStack stack, ItemStack lastItemInMainHand) {
+        if (stack.getItem() instanceof BlockItem blockItem && lastItemInMainHand.getItem() instanceof BlockItem lastBlockItem) {
+            BlockState state = stack.getOrDefault(DataComponents.BLOCK_STATE, BlockItemStateProperties.EMPTY).apply(blockItem.getBlock().defaultBlockState());
+            BlockState lastState = lastItemInMainHand.getOrDefault(DataComponents.BLOCK_STATE, BlockItemStateProperties.EMPTY).apply(lastBlockItem.getBlock().defaultBlockState());
+            if (Minecraft.getInstance().getModelManager().getBlockModelShaper().getBlockModel(state) instanceof CreativeOnlyBakedModelAccess ^ Minecraft.getInstance().getModelManager().getBlockModelShaper().getBlockModel(lastState) instanceof CreativeOnlyBakedModelAccess)
+                refreshBarrierSections();
+        } else if (stack.getItem() instanceof BlockItem blockItem) {
+            BlockState state = stack.getOrDefault(DataComponents.BLOCK_STATE, BlockItemStateProperties.EMPTY).apply(blockItem.getBlock().defaultBlockState());
+            if (Minecraft.getInstance().getModelManager().getBlockModelShaper().getBlockModel(state) instanceof CreativeOnlyBakedModelAccess)
+                refreshBarrierSections();
+        } else if (lastItemInMainHand.getItem() instanceof BlockItem blockItem) {
+            BlockState state = lastItemInMainHand.getOrDefault(DataComponents.BLOCK_STATE, BlockItemStateProperties.EMPTY).apply(blockItem.getBlock().defaultBlockState());
+            if (Minecraft.getInstance().getModelManager().getBlockModelShaper().getBlockModel(state) instanceof CreativeOnlyBakedModelAccess)
+                refreshBarrierSections();
+        }
+    }
+
+    private static void refreshBarrierSections() {
         Set<SectionPos> operatedSectionPos = new HashSet<>();
         var chunks = ((ClientChunkCacheStorageAccessor)(Object)((ClientChunkCacheAccessor) Minecraft.getInstance().level.getChunkSource()).getStorage()).getChunks();
         for (int i = 0; i < chunks.length(); ++i) {
--- common/src/main/java/net/modgarden/barricade/mixin/PlayerMixin.java
+++ common/src/main/java/net/modgarden/barricade/mixin/PlayerMixin.java
@@ -1,16 +1,10 @@
 package net.modgarden.barricade.mixin;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.core.component.DataComponents;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.component.BlockItemStateProperties;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.state.BlockState;
-import net.modgarden.barricade.client.model.CreativeOnlyBakedModelAccess;
 import net.modgarden.barricade.client.util.BarrierRenderUtils;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -31,19 +25,6 @@ public abstract class PlayerMixin extends LivingEntity {
     private void barricade$refreshBarriersWhenSwapping(CallbackInfo ci, int i, double d0, double d1, ItemStack stack) {
         if (!level().isClientSide())
             return;
-        if (stack.getItem() instanceof BlockItem blockItem && lastItemInMainHand.getItem() instanceof BlockItem lastBlockItem) {
-            BlockState state = stack.getOrDefault(DataComponents.BLOCK_STATE, BlockItemStateProperties.EMPTY).apply(blockItem.getBlock().defaultBlockState());
-            BlockState lastState = lastItemInMainHand.getOrDefault(DataComponents.BLOCK_STATE, BlockItemStateProperties.EMPTY).apply(lastBlockItem.getBlock().defaultBlockState());
-            if (Minecraft.getInstance().getModelManager().getBlockModelShaper().getBlockModel(state) instanceof CreativeOnlyBakedModelAccess ^ Minecraft.getInstance().getModelManager().getBlockModelShaper().getBlockModel(lastState) instanceof CreativeOnlyBakedModelAccess)
-                BarrierRenderUtils.reloadBarrierBlocks();
-        } else if (stack.getItem() instanceof BlockItem blockItem) {
-            BlockState state = stack.getOrDefault(DataComponents.BLOCK_STATE, BlockItemStateProperties.EMPTY).apply(blockItem.getBlock().defaultBlockState());
-            if (Minecraft.getInstance().getModelManager().getBlockModelShaper().getBlockModel(state) instanceof CreativeOnlyBakedModelAccess)
-                BarrierRenderUtils.reloadBarrierBlocks();
-        } else if (lastItemInMainHand.getItem() instanceof BlockItem blockItem) {
-            BlockState state = lastItemInMainHand.getOrDefault(DataComponents.BLOCK_STATE, BlockItemStateProperties.EMPTY).apply(blockItem.getBlock().defaultBlockState());
-            if (Minecraft.getInstance().getModelManager().getBlockModelShaper().getBlockModel(state) instanceof CreativeOnlyBakedModelAccess)
-                BarrierRenderUtils.reloadBarrierBlocks();
-        }
+        BarrierRenderUtils.refreshBarrierBlocks(stack, lastItemInMainHand);
     }
 }
