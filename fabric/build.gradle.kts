a5c6f25966fcfe5b023f1bbfb879923c3ba3d5af
sylv256 <contact@sylv.gay>
2025-05-07 18:31:40 -0400

    build(gradle): Enable Mixin hotswapping + Enhanced Class Redefinition (for JBR/DCEVM)

--- fabric/build.gradle.kts
+++ fabric/build.gradle.kts
@@ -59,6 +59,24 @@ loom {
 		}
 	}
 	runs {
+		afterEvaluate {
+			val mixinJarFile = configurations.runtimeClasspath.get().incoming.artifactView {
+				componentFilter {
+					it is ModuleComponentIdentifier && it.group == "net.fabricmc" && it.module == "sponge-mixin"
+				}
+			}.files.first()
+
+			configureEach {
+				// Mixin hotswapping
+				vmArg("-javaagent:$mixinJarFile")
+
+				property("mixin.hotSwap", "true")
+				property("mixin.debug.export", "true")
+
+				// Class redefinition (for JBR/DCEVM)
+				vmArg("-XX:+AllowEnhancedClassRedefinition")
+			}
+		}
 		named("client") {
 			client()
 			configName = "Fabric Client"
