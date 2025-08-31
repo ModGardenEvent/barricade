945cf34ee18b86f0199e531efc3543b933978649
sylv256 <contact@sylv.gay>
2025-08-31 03:02:31 -0400

    fix: only put region once

--- common/src/main/java/net/modgarden/barricade/block/StaticBarrierBlock.java
+++ common/src/main/java/net/modgarden/barricade/block/StaticBarrierBlock.java
@@ -1,13 +1,8 @@
 package net.modgarden.barricade.block;
 
-import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BarrierBlock;
-import net.minecraft.world.level.block.state.BlockState;
-import net.modgarden.barricade.client.renderer.block.BakedRegion;
 import net.modgarden.barricade.data.BlockedDirections;
-import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.HashMap;
@@ -23,16 +18,4 @@ public abstract class StaticBarrierBlock extends BarrierBlock {
 	public abstract BlockedDirections getBlockedDirections();
 
 	public abstract @Nullable ResourceLocation getIcon();
-
-	@Override
-	protected void onPlace(
-			@NotNull BlockState state,
-			@NotNull Level level,
-			@NotNull BlockPos pos,
-			@NotNull BlockState oldState,
-			boolean movedByPiston
-	) {
-		super.onPlace(state, level, pos, oldState, movedByPiston);
-		BakedRegion.putRegion(BakedRegion.BakedRegionPos.fromBlockPos(pos));
-	}
 }
